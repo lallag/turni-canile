83129706081a808b57510a3febe8a8b6ffda9670
lallag <52920632+lallag@users.noreply.github.com>
2026-09-17 10:42:56 +0200

    app_canile.py

--- app_canile.py
+++ app_canile.py
@@ -11,8 +11,8 @@ st.set_page_config(
 st.markdown(
     """
     <head>
-        <link rel="apple-touch-icon" href="icona.jpg">
-        <link rel="icon" type="image/png" href="icona.jpg">
+        <link rel="apple-touch-icon" href="https://github.com/lallag/turni-canile/blob/main/icona.jpg?raw=true">
+        <link rel="icon" type="image/png" href="https://github.com/lallag/turni-canile/blob/main/icona.jpg?raw=true">
     </head>
 """,
     unsafe_allow_html=True,
